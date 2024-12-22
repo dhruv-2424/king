de2dd602bb525b33a8014597adef938f95751f82
dhruv-2424 <126473495+dhruv-2424@users.noreply.github.com>
2024-12-22 17:13:54 +0530

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException, TimeoutException
 import subprocess
 import time
